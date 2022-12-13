b761b9a89c98a9e7b14c09ffe535f1b8a2a70279
DaniOrtega12 <daniel.ortegajimenez@telefonica.com>
2022-12-13 12:39:37 +0100

    component functionality finished

---
+++ src/components/AnimationPrueba/AnimationPrueba.stories.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AnimationPrueba from "./AnimationPrueba";
+// import 'typeface-roboto';
+
+export default {
+    title: "AnimationPrueba",
+    component: AnimationPrueba
+}
+
+export const AnimationPruebaExample = () => <AnimationPrueba></AnimationPrueba>
