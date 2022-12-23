36b49dcfff8beb4f91772b46faae166fe6f63226
daniojz <danioj24@gmail.com>
2022-12-23 20:59:17 +0100

    delete sobre-mi view for only use home view

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./views/home/home";
-import SobreMi from "./views/sobre_mi/SobreMi";
 
 import { Route } from "wouter";
 import TypeBeats from "./views/type_beats/TypeBeats";
@@ -10,16 +9,11 @@ function App() {
   return (
     <div className="App">
       <header>
-        <Route path="/">
-          <Navbar showIcon={false}></Navbar>
-        </Route>
-        <Route path="/sobre-mi">
-          <Navbar showIcon={true}></Navbar>
-        </Route>
+        <Navbar></Navbar>
       </header>
       <main>
         <Route path="/" component={Home} />
-        <Route path="/sobre-mi" component={SobreMi} />
+        <Route path="/home" component={Home} />
         <Route path="/type-beats" component={TypeBeats} />
       </main>
       <Route path="/sobre-mi" component={Footer}></Route>
