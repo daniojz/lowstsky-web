46cfa5feb2892bd45209631cd1c645790439023f
daniojz <danioj24@gmail.com>
2022-12-30 14:56:06 +0100

    progress last releases section

--- src/pages/Home/views/LastReleases/LastReleases.jsx
+++ src/pages/Home/views/LastReleases/LastReleases.jsx
@@ -12,6 +12,7 @@ const LastReleases = ({}) => {
         <SongCover imgUrl={t('lastReleases.songCover1.imgUrl')} pageUrl={t('lastReleases.songCover1.pageUrl')}></SongCover>
         <SongCover imgUrl={t('lastReleases.songCover2.imgUrl')} pageUrl={t('lastReleases.songCover2.pageUrl')}></SongCover>
         <SongCover imgUrl={t('lastReleases.songCover3.imgUrl')} pageUrl={t('lastReleases.songCover3.pageUrl')}></SongCover>
+        <SongCover imgUrl={t('lastReleases.songCover3.imgUrl')} pageUrl={t('lastReleases.songCover4.pageUrl')}></SongCover>
       </div>
     </div>
   );
